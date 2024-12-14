2fd439b2c864c033eb5c7a11b47268f166099f34
Carlos Ricardo Machado Oliveira <carlos-ricardo.oliveira@serpro.gov.br>
2024-12-14 00:28:33 -0300

    file set to release 1.0.0

--- src/data_check/test_data.py
+++ src/data_check/test_data.py
@@ -59,7 +59,13 @@ def test_similar_neigh_distrib(data: pd.DataFrame, ref_data: pd.DataFrame, kl_th
 
     assert scipy.stats.entropy(dist1, dist2, base=2) < kl_threshold
 
+def test_row_count(data):
+    assert 15000 < data.shape[0] < 1000000
 
+
+def test_prince_range(data, min_price, max_price):
+    # assert data['price'].between(min_price,max_price).all()
+    assert data['price'].between(min_price,max_price).all()
 ########################################################
 # Implement here test_row_count and test_price_range   #
 ########################################################
